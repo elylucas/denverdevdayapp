3f21a407f082a188f75dc6d112ab7feb4f7fa463
Ely Lucas <ely@meta-tek.net>
2018-04-22 09:15:36 -0600

    status bar fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
       splashScreen.hide();
     });
   }
